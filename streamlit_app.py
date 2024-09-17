e26b03a831c35a55681e640524b16eecaf8350f9
BettingIsCool <contact@bettingiscool.com>
2024-09-17 20:58:30 +0200

    Add authentication to Streamlit app
    Integrated the `add_auth` function from `st_paywall` to require authentication. This change ensures that users must be authenticated to access the app, enhancing security. Additionally, updated the sidebar logo display settings for improved visual consistency.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from st_paywall import add_auth
 from config import TEXT1_LANDING_PAGE, TEXT2_LANDING_PAGE
 
 placeholder1 = st.empty()
@@ -8,5 +9,7 @@ placeholder3 = st.empty()
 placeholder1.markdown(TEXT1_LANDING_PAGE)
 placeholder2.image(image="dashboard.png")
 placeholder3.markdown(TEXT2_LANDING_PAGE)
+
+add_auth(required=True)
 
 st.sidebar.image(image="logo_sbic_round.png", use_column_width='auto')
