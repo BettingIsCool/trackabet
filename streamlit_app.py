ddb0a773d0ba33bb413e402e3fe103347f5c5b73
BettingIsCool <contact@bettingiscool.com>
2024-09-18 15:26:53 +0200

    Add equality operator for bet deletion condition
    Updated the condition for detecting bets marked for deletion to use the equality operator `==` instead of `is`. This ensures that the correct rows are identified for deletion when users interact with the `DELETE` checkbox. Additionally, a new TODO item for setting up a private GitHub repo for Streamlit teams was added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ from config import SPORTS, PERIODS, BOOKS, TEXT1_LANDING_PAGE, TEXT2_LANDING_PAG
 # TODO script runs top-to-bottom on EVERY user interaction, exlude unnecessary tasks (i.e. get_users(), display landing_page_text, etc.)
 # TODO filter for graded/ungraded bets
 # TODO import @pyckio picks (complete database)
+# TODO private github repo (streamlit teams)
 
 
 # Display landing page (pre login)
@@ -221,7 +222,7 @@ if selected_sports != '()':
                 styled_df = bets_df.style.applymap(tools.color_cells, subset=['ST', 'P/L', 'EXP_WIN', 'CLV']).format({'LINE': '{:g}'.format, 'ODDS': '{:,.3f}'.format, 'STAKE': '{:,.2f}'.format, 'P/L': '{:,.2f}'.format, 'CLS': '{:,.3f}'.format, 'CLS_TRUE': '{:,.3f}'.format, 'CLS_LIMIT': '{:,.0f}'.format, 'EXP_WIN': '{:,.2f}'.format, 'CLV': '{:,.2%}'.format, 'SH': '{0:g}'.format, 'SA': '{0:g}'.format})
                 df = st.data_editor(styled_df, column_config={"DEL": st.column_config.CheckboxColumn("DEL", help="Select if you want to delete this bet.", default=False)}, disabled=['TAG', 'STARTS', 'SPORT', 'LEAGUE', 'RUNNER_HOME', 'RUNNER_AWAY', 'MARKET', 'PERIOD', 'SIDE', 'LINE', 'ODDS', 'STAKE', 'BOOK', 'ST', 'SH', 'SA', 'P/L', 'CLS', 'CLS_TRUE', 'CLS_LIMIT', 'EXP_WIN', 'CLV', 'BET_ADDED', 'ID'], hide_index=True)
 
-                bets_to_be_deleted = df.loc[(df['DEL'] is True), 'ID'].tolist()
+                bets_to_be_deleted = df.loc[(df['DEL'] == True), 'ID'].tolist()
 
 if bets_to_be_deleted:
     st.button('Delete selected bet(s)', on_click=tools.delete_bets, args=(bets_to_be_deleted,), type="primary")
