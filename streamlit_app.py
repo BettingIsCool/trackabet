f523805d5cd6639fcf350e5fabc81e678ebda048
BettingIsCool <contact@bettingiscool.com>
2024-09-26 14:58:20 +0200

    Fix timezone conversion issue in event options display.
    Replaced timezone-aware datetime with a naive datetime to fix time formatting issue in event options display. This ensures consistent display of event start times across different user timezones.

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,8 +101,7 @@ if st.session_state.session_id == tools.get_active_session():
                 for index, row in events.iterrows():
                     if row['event_id'] not in event_options.keys():
                         starts_converted_to_timezone = pytz.timezone('Europe/Vienna').localize(row['starts']).astimezone(pytz.timezone(st.session_state.timezone)).replace(tzinfo=None)
-                        st.write(starts_converted_to_timezone.replace(second=0))
-                        st.write(starts_converted_to_timezone)
+                        st.write(type(starts_converted_to_timezone))
                         event_options.update({row['event_id']: f"{starts_converted_to_timezone.replace(second=0)} {row['league_name'].upper()} {row['runner_home']} - {row['runner_away']}"})
                         event_details.update({row['event_id']: {'starts': row['starts'], 'league_id': row['league_id'], 'league_name': row['league_name'], 'runner_home': row['runner_home'], 'runner_away': row['runner_away']}})
                 selected_event_id = st.sidebar.selectbox(label='Select event', options=event_options.keys(), index=None, format_func=lambda x: event_options.get(x), placeholder='Start typing...', help='Start searching your fixture by typing any league, home team, away team. Only fixtures with available odds are listed.')
