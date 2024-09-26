1458a4b8c5ae4fce2006d44eacc5b00fe98f8fcd
BettingIsCool <contact@bettingiscool.com>
2024-09-26 15:16:55 +0200

    Convert dates to Europe/Vienna timezone in get_fixtures call.
    Adjusted `selected_from_date` and `selected_to_date` variables to be converted to the Europe/Vienna timezone before calling the `get_fixtures` method. This ensures consistency in date handling across different user timezones.

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,10 @@ if st.session_state.session_id == tools.get_active_session():
             # This string is what users see in the dropdown menu
             if selected_to_date:
                 runtime_start = time.time()
-                events = db.get_fixtures(sport_id=SPORTS[selected_sport], date_from=selected_from_date, date_to=selected_to_date)
+                selected_from_date_converted_to_timezone = pytz.timezone(st.session_state.timezone).localize(selected_from_date).astimezone(pytz.timezone('Europe/Vienna'))
+                selected_to_date_converted_to_timezone = pytz.timezone(st.session_state.timezone).localize(selected_to_date).astimezone(pytz.timezone('Europe/Vienna'))
+
+                events = db.get_fixtures(sport_id=SPORTS[selected_sport], date_from=selected_from_date_converted_to_timezone, date_to=selected_to_date_converted_to_timezone)
 
                 st.write(f"Runtime get_fixtures: {round(time.time() - runtime_start, 3)} seconds.")
 
