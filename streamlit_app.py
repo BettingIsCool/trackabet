0f09a82f1bedcf8e3f3a9030b3dfbcb303113087
BettingIsCool <contact@bettingiscool.com>
2024-09-29 16:32:06 +0200

    Add state management and update validation for data editor
    This commit introduces functions to manage the state of the dataframe and validate updates. It ensures that changes to the dataframe are tracked and stored in session state, improving the consistency and integrity of the edited data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -302,6 +302,8 @@ if st.session_state.session_id == tools.get_active_session():
                                 previous_status = row['ST']
                                 current_status = edited_df[edited_df['ID'] == row['ID']]['ST'].iloc[0]
 
+                                st.write(previous_status, current_status)
+
                                 if previous_status == 'na' and current_status != 'na':
 
                                     st.info.write('You can not change the status of an unsettled event. Please wait for the event to be graded and then try again.')
