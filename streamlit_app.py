b065c5443c8236f9b46f5d7cfd3813c356c5bdce
BettingIsCool <contact@bettingiscool.com>
2024-09-29 23:06:28 +0200

    Fix data handling in bet editor
    Corrected the assignment of session state variables for the data editor component to ensure accurate data storage and manipulation. Updated references in the deletion logic to align with the modified session state usage.

--- streamlit_app.py
+++ streamlit_app.py
@@ -327,7 +327,9 @@ if st.session_state.session_id == tools.get_active_session():
                         if not st.session_state['initial_df'].equals(st.session_state['edited_df']):
                             
                             update(st.session_state['initial_df'], st.session_state['edited_df'])
-                            st.session_state['initial_df'] = st.session_state['edited_df']
+
+                        st.session_state['initial_df'] = st.session_state['edited_df']
+                        st.write(st.session_state['initial_df'].equals(st.session_state['edited_df']))
                             
                             
                             
